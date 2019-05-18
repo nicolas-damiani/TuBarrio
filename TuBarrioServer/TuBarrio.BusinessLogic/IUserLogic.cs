4bfb54c0ff8a903290cc2b55e5e3a4f898be85f9
Gkurin <gonzalokurin@gmail.com>
2019-05-18 18:21:48 -0300

    IUserRepository

--- TuBarrioServer/TuBarrio.BusinessLogic/IUserLogic.cs
+++ TuBarrioServer/TuBarrio.BusinessLogic/IUserLogic.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TuBarrio.Entities;
 
 namespace TuBarrio.BusinessLogic
 {
-    public class Class1
+    public interface IUserLogic
     {
+        
     }
 }
